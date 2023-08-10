28ee6c7c81c573d5a47f15d6575a81603a9c4c4b
Vikas <vikas3517@gmail.com>
2023-08-10 21:29:23 +0530

    Intialization

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Println("hello world")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+	fmt.Println("Port: ",portString)
+}
